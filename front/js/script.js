f4fb73ad35654ae1d1eb7dbd65cbdc13072789a2
CarolinaTartarotiShishito <24.00599-0@maua.br>
2024-10-18 09:12:52 -0300

    até estrutura para o novo filme

--- front/js/script.js
+++ front/js/script.js
@@ -5,6 +5,20 @@ const filmesEndPoint = '/filmes'
 async function obterFilmes() {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
     const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    
+    for (let filme of filmes){ //vai inserindo linha no início da tabela, empilhando
+	    let linha = corpoTabela.insertRow(0) 
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
     
     console.log(filmes)
+}
+
+function cadastrarFilme() {
+    console.log('vamos cadastrar')
 }
